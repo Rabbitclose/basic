b12bbcb323d9fc662a2f1860d550777f24f0679d
Rabbitclose <29276773@s.mfua.ru>
2024-02-18 23:30:54 +0300

    On branch main

--- controllers/TelegramController.php
+++ controllers/TelegramController.php
@@ -7,20 +7,20 @@ class TelegramController extends Controller{
         $message= Telegram::find()->all();
         return $this->render('index', compact('message'));
     }
-        public function actionTextMessage(){
-            $res = \Yii::$app->telegram->sendMessage([
-                'chat_id' => $chat_id,
-                'text' => 'hello world!!' 
-                ]);
-                }
-                public function actionSendMessage($idTelegram,$message){
-                    Telegram::sendMessage((int)$idTelegram,$message);
-                    }
-                    public function actionGetAllMesages(){
-                        $listMessages = Telegram::getMessages();
-                         return $this->render('contact', [
-                                'listMessages'=> $listMessages
-                            ]);
-                        }
+        // public function actionTextMessage(){
+        //     $res = \Yii::$app->telegram->sendMessage([
+        //         'chat_id' => $chat_id,
+        //         'text' => 'hello world!!' 
+        //         ]);
+        //         }
+        //         public function actionSendMessage($idTelegram,$message){
+        //             Telegram::sendMessage((int)$idTelegram,$message);
+        //             }
+        //             public function actionGetAllMesages(){
+        //                 $listMessages = Telegram::getMessages();
+        //                  return $this->render('contact', [
+        //                         'listMessages'=> $listMessages
+        //                     ]);
+        //                 }
 
 }
