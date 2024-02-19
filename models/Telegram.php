6cf2c00564cde337ce6d99cb741306a9e54ceb50
Rabbitclose <96257163+Rabbitclose@users.noreply.github.com>
2024-02-19 10:26:13 +0300

    Update Telegram.php

--- models/Telegram.php
+++ models/Telegram.php
@@ -12,14 +12,20 @@ const TELEGRAM_BOT_URL = 'https://api.telega......';
 * @var $id int  айди тово кому отправка идёт
 * @var $message string что имеено отправляем
 */    
-public static function sendMessage($id,$message){
-	$ch = curl_init('https://example.com'.self::TELEGRAM_BOT_ID);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_HEADER, false);
-	$html = curl_exec($ch);
-	curl_close($ch);
-	return $html;
+public static function sendMessage($id=null,$message=null){
+	try{
+		if($id != null and $message!=null){
+			$ch = curl_init('https://example.com'.self::TELEGRAM_BOT_ID);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_HEADER, false);
+			$html = curl_exec($ch);
+			curl_close($ch);
+			return $html;
+		}
+	}catch(\Exeption $e){
+		echo $e->getMessages();	
+		}
 	}
 /**
 * @fun getMessages() выводит все собщения которые пришли в бот!
