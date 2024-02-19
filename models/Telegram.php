fc973d8c98c79850572def2c29b793c0ec7e85cb
Rabbitclose <96257163+Rabbitclose@users.noreply.github.com>
2024-02-19 10:11:17 +0300

    Update Telegram.php

--- models/Telegram.php
+++ models/Telegram.php
@@ -1,12 +1,22 @@
 <?php
 namespace app\models;
 
-use yii\db\ActiveRecord;
 
-class Telegram extends ActiveRecord{
-    public static function tableName(){
+class Telegram {
+const TELEGRAM_BOT_ID = 'FYTURFRF67IR67IR67FRFTYFDTDRED';
+/**
+* Отправка сообщений через ТГ
+* @var $id int  айди тово кому отправка идёт
+* @var $message string что имеено отправляем
+*/    
+public static function sendMessage($id,$message){
+	$ch = curl_init('https://example.com'.self::TELEGRAM_BOT_ID);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	$html = curl_exec($ch);
+	curl_close($ch);
+	return $html;
+	}
 
-        return "telegram";
-
-}
 }
