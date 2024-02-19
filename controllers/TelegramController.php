b1d7b914df885dd94e5a2eff9d70cad142ab921f
Rabbitclose <96257163+Rabbitclose@users.noreply.github.com>
2024-02-19 10:14:09 +0300

    Update TelegramController.php

--- controllers/TelegramController.php
+++ controllers/TelegramController.php
@@ -3,6 +3,10 @@ namespace app\controllers;
 use app\models\Telegram;
 use yii\web\Controller;
 class TelegramController extends Controller{
+    /**
+     * @var $idTelegram int айдишник кому шлём
+     * @var $message string сообщение что хотим передать
+     */
     public function actionIndex($idTelegram,$message){
         $result = Telegram::sendMessage($idTelegram,$message);
         return $this->render('index', [' result'=>$result]);
